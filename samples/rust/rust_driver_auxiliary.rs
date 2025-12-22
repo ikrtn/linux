5c151099d2f14e55f0d52062cf15ebef3850ca7b
Tamir Duberstein <tamird@gmail.com>
2025-12-22 13:35:27 +0100

    rust: auxiliary: replace `kernel::c_str!` with C-Strings
    C-String literals were added in Rust 1.77. Replace instances of
    `kernel::c_str!` with C-String literals where possible.
    
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Benno Lossin <lossin@kernel.org>
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Link: https://patch.msgid.link/20251222-cstr-driver-core-v1-1-1142a177d0fd@gmail.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- samples/rust/rust_driver_auxiliary.rs
+++ samples/rust/rust_driver_auxiliary.rs
@@ -5,7 +5,7 @@
 //! To make this driver probe, QEMU must be run with `-device pci-testdev`.
 
 use kernel::{
-    auxiliary, c_str,
+    auxiliary,
     device::{Bound, Core},
     devres::Devres,
     driver,
@@ -18,7 +18,7 @@ use kernel::{
 use core::any::TypeId;
 
 const MODULE_NAME: &CStr = <LocalModule as kernel::ModuleMetadata>::NAME;
-const AUXILIARY_NAME: &CStr = c_str!("auxiliary");
+const AUXILIARY_NAME: &CStr = c"auxiliary";
 
 struct AuxiliaryDriver;
 
