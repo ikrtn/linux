b406110fc0691a3ff2d120c25fd2d2525e7ce039
Igor Korotin <igor.korotin.linux@gmail.com>
2026-01-10 17:41:04 +0000

    rust: i2c: split client and adapter code into separate files
    Reorganize the I2C module by splitting client-related and
    adapter-related code into separate files for better maintainability.
    
    - Move client traits and implementations to i2c/client.rs
    - Move adapter traits and implementations to i2c/adapter.rs
    
    Signed-off-by: Igor Korotin <igor.korotin.linux@gmail.com>

--- rust/kernel/i2c.rs
+++ rust/kernel/i2c.rs
@@ -1,37 +1,25 @@
 // SPDX-License-Identifier: GPL-2.0
 
-//! I2C Driver subsystem
+//! I2C subsystem
 
 // I2C Driver abstractions.
 use crate::{
     acpi,
-    container_of,
     device,
     device_id::{
         RawDeviceId,
         RawDeviceIdIndex, //
     },
-    devres::Devres,
     driver,
     error::*,
+    i2c::client::I2cClient,
     of,
     prelude::*,
-    types::{
-        AlwaysRefCounted,
-        Opaque, //
-    }, //
+    types::Opaque, //
 };
 
-use core::{
-    marker::PhantomData,
-    mem::offset_of,
-    ptr::{
-        from_ref,
-        NonNull, //
-    }, //
-};
-
-use kernel::types::ARef;
+pub mod adapter;
+pub mod client;
 
 /// An I2C device id table.
 #[repr(transparent)]
@@ -291,13 +279,13 @@ macro_rules! module_i2c_driver {
 ///     const ACPI_ID_TABLE: Option<acpi::IdTable<Self::IdInfo>> = Some(&ACPI_TABLE);
 ///
 ///     fn probe(
-///         _idev: &i2c::I2cClient<Core>,
+///         _idev: &i2c::client::I2cClient<Core>,
 ///         _id_info: Option<&Self::IdInfo>,
 ///     ) -> impl PinInit<Self, Error> {
 ///         Err(ENODEV)
 ///     }
 ///
-///     fn shutdown(_idev: &i2c::I2cClient<Core>, this: Pin<&Self>) {
+///     fn shutdown(_idev: &i2c::client::I2cClient<Core>, this: Pin<&Self>) {
 ///     }
 /// }
 ///```
@@ -357,230 +345,3 @@ pub trait Driver: Send {
         let _ = (dev, this);
     }
 }
-
-/// The i2c adapter representation.
-///
-/// This structure represents the Rust abstraction for a C `struct i2c_adapter`. The
-/// implementation abstracts the usage of an existing C `struct i2c_adapter` that
-/// gets passed from the C side
-///
-/// # Invariants
-///
-/// A [`I2cAdapter`] instance represents a valid `struct i2c_adapter` created by the C portion of
-/// the kernel.
-#[repr(transparent)]
-pub struct I2cAdapter<Ctx: device::DeviceContext = device::Normal>(
-    Opaque<bindings::i2c_adapter>,
-    PhantomData<Ctx>,
-);
-
-impl<Ctx: device::DeviceContext> I2cAdapter<Ctx> {
-    fn as_raw(&self) -> *mut bindings::i2c_adapter {
-        self.0.get()
-    }
-}
-
-impl I2cAdapter {
-    /// Returns the I2C Adapter index.
-    #[inline]
-    pub fn index(&self) -> i32 {
-        // SAFETY: `self.as_raw` is a valid pointer to a `struct i2c_adapter`.
-        unsafe { (*self.as_raw()).nr }
-    }
-
-    /// Gets pointer to an `i2c_adapter` by index.
-    pub fn get(index: i32) -> Result<ARef<Self>> {
-        // SAFETY: `index` must refer to a valid I2C adapter; the kernel
-        // guarantees that `i2c_get_adapter(index)` returns either a valid
-        // pointer or NULL. `NonNull::new` guarantees the correct check.
-        let adapter = NonNull::new(unsafe { bindings::i2c_get_adapter(index) }).ok_or(ENODEV)?;
-
-        // SAFETY: `adapter` is non-null and points to a live `i2c_adapter`.
-        // `I2cAdapter` is #[repr(transparent)], so this cast is valid.
-        Ok(unsafe { (&*adapter.as_ptr().cast::<I2cAdapter<device::Normal>>()).into() })
-    }
-}
-
-// SAFETY: `I2cAdapter` is a transparent wrapper of a type that doesn't depend on
-// `I2cAdapter`'s generic argument.
-kernel::impl_device_context_deref!(unsafe { I2cAdapter });
-kernel::impl_device_context_into_aref!(I2cAdapter);
-
-// SAFETY: Instances of `I2cAdapter` are always reference-counted.
-unsafe impl crate::types::AlwaysRefCounted for I2cAdapter {
-    fn inc_ref(&self) {
-        // SAFETY: The existence of a shared reference guarantees that the refcount is non-zero.
-        unsafe { bindings::i2c_get_adapter(self.index()) };
-    }
-
-    unsafe fn dec_ref(obj: NonNull<Self>) {
-        // SAFETY: The safety requirements guarantee that the refcount is non-zero.
-        unsafe { bindings::i2c_put_adapter(obj.as_ref().as_raw()) }
-    }
-}
-
-/// The i2c board info representation
-///
-/// This structure represents the Rust abstraction for a C `struct i2c_board_info` structure,
-/// which is used for manual I2C client creation.
-#[repr(transparent)]
-pub struct I2cBoardInfo(bindings::i2c_board_info);
-
-impl I2cBoardInfo {
-    const I2C_TYPE_SIZE: usize = 20;
-    /// Create a new [`I2cBoardInfo`] for a kernel driver.
-    #[inline(always)]
-    pub const fn new(type_: &'static CStr, addr: u16) -> Self {
-        let src = type_.to_bytes_with_nul();
-        build_assert!(src.len() <= Self::I2C_TYPE_SIZE, "Type exceeds 20 bytes");
-        let mut i2c_board_info: bindings::i2c_board_info = pin_init::zeroed();
-        let mut i: usize = 0;
-        while i < src.len() {
-            i2c_board_info.type_[i] = src[i];
-            i += 1;
-        }
-
-        i2c_board_info.addr = addr;
-        Self(i2c_board_info)
-    }
-
-    fn as_raw(&self) -> *const bindings::i2c_board_info {
-        from_ref(&self.0)
-    }
-}
-
-/// The i2c client representation.
-///
-/// This structure represents the Rust abstraction for a C `struct i2c_client`. The
-/// implementation abstracts the usage of an existing C `struct i2c_client` that
-/// gets passed from the C side
-///
-/// # Invariants
-///
-/// A [`I2cClient`] instance represents a valid `struct i2c_client` created by the C portion of
-/// the kernel.
-#[repr(transparent)]
-pub struct I2cClient<Ctx: device::DeviceContext = device::Normal>(
-    Opaque<bindings::i2c_client>,
-    PhantomData<Ctx>,
-);
-
-impl<Ctx: device::DeviceContext> I2cClient<Ctx> {
-    fn as_raw(&self) -> *mut bindings::i2c_client {
-        self.0.get()
-    }
-}
-
-// SAFETY: `I2cClient` is a transparent wrapper of `struct i2c_client`.
-// The offset is guaranteed to point to a valid device field inside `I2cClient`.
-unsafe impl<Ctx: device::DeviceContext> device::AsBusDevice<Ctx> for I2cClient<Ctx> {
-    const OFFSET: usize = offset_of!(bindings::i2c_client, dev);
-}
-
-// SAFETY: `I2cClient` is a transparent wrapper of a type that doesn't depend on
-// `I2cClient`'s generic argument.
-kernel::impl_device_context_deref!(unsafe { I2cClient });
-kernel::impl_device_context_into_aref!(I2cClient);
-
-// SAFETY: Instances of `I2cClient` are always reference-counted.
-unsafe impl AlwaysRefCounted for I2cClient {
-    fn inc_ref(&self) {
-        // SAFETY: The existence of a shared reference guarantees that the refcount is non-zero.
-        unsafe { bindings::get_device(self.as_ref().as_raw()) };
-    }
-
-    unsafe fn dec_ref(obj: NonNull<Self>) {
-        // SAFETY: The safety requirements guarantee that the refcount is non-zero.
-        unsafe { bindings::put_device(&raw mut (*obj.as_ref().as_raw()).dev) }
-    }
-}
-
-impl<Ctx: device::DeviceContext> AsRef<device::Device<Ctx>> for I2cClient<Ctx> {
-    fn as_ref(&self) -> &device::Device<Ctx> {
-        let raw = self.as_raw();
-        // SAFETY: By the type invariant of `Self`, `self.as_raw()` is a pointer to a valid
-        // `struct i2c_client`.
-        let dev = unsafe { &raw mut (*raw).dev };
-
-        // SAFETY: `dev` points to a valid `struct device`.
-        unsafe { device::Device::from_raw(dev) }
-    }
-}
-
-impl<Ctx: device::DeviceContext> TryFrom<&device::Device<Ctx>> for &I2cClient<Ctx> {
-    type Error = kernel::error::Error;
-
-    fn try_from(dev: &device::Device<Ctx>) -> Result<Self, Self::Error> {
-        // SAFETY: By the type invariant of `Device`, `dev.as_raw()` is a valid pointer to a
-        // `struct device`.
-        if unsafe { bindings::i2c_verify_client(dev.as_raw()).is_null() } {
-            return Err(EINVAL);
-        }
-
-        // SAFETY: We've just verified that the type of `dev` equals to
-        // `bindings::i2c_client_type`, hence `dev` must be embedded in a valid
-        // `struct i2c_client` as guaranteed by the corresponding C code.
-        let idev = unsafe { container_of!(dev.as_raw(), bindings::i2c_client, dev) };
-
-        // SAFETY: `idev` is a valid pointer to a `struct i2c_client`.
-        Ok(unsafe { &*idev.cast() })
-    }
-}
-
-// SAFETY: A `I2cClient` is always reference-counted and can be released from any thread.
-unsafe impl Send for I2cClient {}
-
-// SAFETY: `I2cClient` can be shared among threads because all methods of `I2cClient`
-// (i.e. `I2cClient<Normal>) are thread safe.
-unsafe impl Sync for I2cClient {}
-
-/// The registration of an i2c client device.
-///
-/// This type represents the registration of a [`struct i2c_client`]. When an instance of this
-/// type is dropped, its respective i2c client device will be unregistered from the system.
-///
-/// # Invariants
-///
-/// `self.0` always holds a valid pointer to an initialized and registered
-/// [`struct i2c_client`].
-#[repr(transparent)]
-pub struct Registration(NonNull<bindings::i2c_client>);
-
-impl Registration {
-    /// The C `i2c_new_client_device` function wrapper for manual I2C client creation.
-    pub fn new<'a>(
-        i2c_adapter: &I2cAdapter,
-        i2c_board_info: &I2cBoardInfo,
-        parent_dev: &'a device::Device<device::Bound>,
-    ) -> impl PinInit<Devres<Self>, Error> + 'a {
-        Devres::new(parent_dev, Self::try_new(i2c_adapter, i2c_board_info))
-    }
-
-    fn try_new(i2c_adapter: &I2cAdapter, i2c_board_info: &I2cBoardInfo) -> Result<Self> {
-        // SAFETY: the kernel guarantees that `i2c_new_client_device()` returns either a valid
-        // pointer or NULL. `from_err_ptr` separates errors. Following `NonNull::new`
-        // checks for NULL.
-        let raw_dev = from_err_ptr(unsafe {
-            bindings::i2c_new_client_device(i2c_adapter.as_raw(), i2c_board_info.as_raw())
-        })?;
-
-        let dev_ptr = NonNull::new(raw_dev).ok_or(ENODEV)?;
-
-        Ok(Self(dev_ptr))
-    }
-}
-
-impl Drop for Registration {
-    fn drop(&mut self) {
-        // SAFETY: `Drop` is only called for a valid `Registration`, which by invariant
-        // always contains a non-null pointer to an `i2c_client`.
-        unsafe { bindings::i2c_unregister_device(self.0.as_ptr()) }
-    }
-}
-
-// SAFETY: A `Registration` of a `struct i2c_client` can be released from any thread.
-unsafe impl Send for Registration {}
-
-// SAFETY: `Registration` offers no interior mutability (no mutation through &self
-// and no mutable access is exposed)
-unsafe impl Sync for Registration {}
